916f7de4e0373a317aeb888205201d1f4538f7b9
Daniel Howlett <dan@dancras.co.uk>
2019-10-22 15:52:02 +0200

    Add crude interface to pause and reset collision example

--- examples/collision_ball_ball_rear.rs
+++ examples/collision_ball_ball_rear.rs
@@ -1,7 +1,7 @@
 use ggez::{Context, ContextBuilder, GameResult};
 use ggez::graphics;
 use ggez::conf::{WindowMode};
-use ggez::event::{self, EventHandler};
+use ggez::event::{self, EventHandler, KeyCode, KeyMods};
 use nalgebra::{Point2, Vector2};
 use crasball::game::{Ball,GameState};
 
@@ -23,38 +23,55 @@ fn main() {
 }
 
 struct CrasballGame {
+    is_paused: bool,
     state: GameState
+}
+
+fn create_initial_game_state() -> GameState {
+    GameState {
+        balls: vec![
+            Ball {
+                radius: 20.0,
+                position: Point2::new(100.0, 200.0),
+                movement: Vector2::new(100.0, 0.0)
+            },
+            Ball {
+                radius: 20.0,
+                position: Point2::new(141.0, 200.0),
+                movement: Vector2::new(
+                    (5000.0 as f32).sqrt(),
+                    (5000.0 as f32).sqrt()
+                )
+            }
+        ],
+        walls: Vec::new()
+    }
 }
 
 impl CrasballGame {
     pub fn new(_ctx: &mut Context) -> CrasballGame {
-        // Load/create resources such as images here.
         CrasballGame {
-            state: GameState {
-                balls: vec![
-                    Ball {
-                        radius: 20.0,
-                        position: Point2::new(100.0, 200.0),
-                        movement: Vector2::new(100.0, 0.0)
-                    },
-                    Ball {
-                        radius: 20.0,
-                        position: Point2::new(141.0, 200.0),
-                        movement: Vector2::new(
-                            (5000.0 as f32).sqrt(),
-                            (5000.0 as f32).sqrt()
-                        )
-                    }
-                ],
-                walls: Vec::new()
-            }
+            is_paused: true,
+            state: create_initial_game_state()
         }
     }
 }
 
 impl EventHandler for CrasballGame {
+
+    fn key_down_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymod: KeyMods, _repeat: bool) {
+        match keycode {
+            KeyCode::Space => self.is_paused = !self.is_paused,
+            KeyCode::R => self.state = create_initial_game_state(),
+            _ => (),
+        }
+    }
+
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-        self.state.update(ctx);
+        if !self.is_paused {
+            self.state.update(ctx);
+        }
+
         Ok(())
     }
 
